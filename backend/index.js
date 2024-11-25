3847d037d0997b2a699586afd56dc4fb287fe01d
anusha mahajan <99132058+anushamahajan5@users.noreply.github.com>
2024-11-26 02:23:11 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,10 @@ const cors = require('cors');
 app.get('/', (req, res) => {
     res.send('Hello from the server');
 });
-app.use(cors())
+app.use(cors({
+    origin: '*', // Allows requests from any origin
+}));
+
 app.use(bodyParser.json());
 app.use('/tasks', TaskRouter)
 
