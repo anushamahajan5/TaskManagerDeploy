838c42ce4719a50722cadf191b8e19899326a783
anusha mahajan <99132058+anushamahajan5@users.noreply.github.com>
2024-11-26 01:54:38 +0530

    Update utils.js

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -4,5 +4,5 @@ export const notify = (message, type) => {
     toast[type](message);
 }
 
-export const API_URL = 'http://localhost:8080';
-//'https://mern-task-manager-app-deploy-api.vercel.app';
+export const API_URL = 'https://task-manager-deploy-api.vercel.app';
+//http://localhost:8080';
